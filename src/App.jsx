cc8298cee8b4d7ee6870b785456be87c02c446dd
Elier Jaque <eliertem@gmail.com>
2024-12-20 00:10:03 -0300

    pizza_mama_mia

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Home from './components/Home'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from './components/Footer'
+import Navbarra from './components/Navbarra'
+
+
+
+
+
+function App() {
+ 
+
+  return (
+    <div>
+      <Navbarra/>
+      <Home />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
